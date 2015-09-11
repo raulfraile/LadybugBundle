778d644aeaa303c01e0774f68a76a8dcb9fe9e67
samrodriguez <melitor@gmail.com>
2015-09-11 09:17:19 -0600

    DEPRECATION - The Twig_Filter_Function
    DEPRECATION - The Twig_Filter_Function class is deprecated since version 1.12 and will be removed in 2.0. Use Twig_SimpleFilter instead.

--- Twig/Extension/LadybugExtension.php
+++ Twig/Extension/LadybugExtension.php
@@ -34,8 +34,8 @@ class LadybugExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'ladybug_dump' => new \Twig_Filter_Method($this, 'ladybug_dump', array('is_safe' => array('html'))),
-            'ld'  => new \Twig_Filter_Method($this, 'ladybug_dump', array('is_safe' => array('html')))
+            new \Twig_SimpleFilter('ladybug_dump',array($this, 'ladybug_dump', array('is_safe' => array('html')))),
+            new \Twig_SimpleFilter('ld',array($this, 'ladybug_dump', array('is_safe' => array('html')))),
         );
     }
 
@@ -47,8 +47,8 @@ class LadybugExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'ladybug_dump' => new \Twig_Function_Method($this, 'ladybug_dump', array('is_safe' => array('html'))),
-            'ld'  => new \Twig_Function_Method($this, 'ladybug_dump', array('is_safe' => array('html')))
+            new \Twig_SimpleFilter('ladybug_dump',array($this, 'ladybug_dump', array('is_safe' => array('html')))),
+            new \Twig_SimpleFilter('ld',array($this, 'ladybug_dump', array('is_safe' => array('html'))))
         );
     }
 
