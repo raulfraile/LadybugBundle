43f8a3d2e3cfef86c8a9a63ead41032bb38f256d
Jorge García <jorge.garcia@fon.com>
2015-12-10 12:29:52 +0100

    Use new Twig filter and function extension methods

--- Twig/Extension/LadybugExtension.php
+++ Twig/Extension/LadybugExtension.php
@@ -34,8 +34,8 @@ class LadybugExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'ladybug_dump' => new \Twig_Filter_Method($this, 'ladybug_dump', array('is_safe' => array('html'))),
-            'ld'  => new \Twig_Filter_Method($this, 'ladybug_dump', array('is_safe' => array('html')))
+            'ladybug_dump' => new \Twig_SimpleFilter($this, 'ladybug_dump', array('is_safe' => array('html'))),
+            'ld'  => new \Twig_SimpleFilter($this, 'ladybug_dump', array('is_safe' => array('html')))
         );
     }
 
@@ -47,8 +47,8 @@ class LadybugExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'ladybug_dump' => new \Twig_Function_Method($this, 'ladybug_dump', array('is_safe' => array('html'))),
-            'ld'  => new \Twig_Function_Method($this, 'ladybug_dump', array('is_safe' => array('html')))
+            'ladybug_dump' => new \Twig_SimpleFunction($this, 'ladybug_dump', array('is_safe' => array('html'))),
+            'ld'  => new \Twig_SimpleFunction($this, 'ladybug_dump', array('is_safe' => array('html')))
         );
     }
 
